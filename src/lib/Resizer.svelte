0afbf518c784c5708200ba29634d0f49c1cc5c8f
Doug A <douganderson444@gmail.com>
2022-06-17 19:30:16 -0300

    rebuild resizer

--- src/lib/Resizer.svelte
+++ src/lib/Resizer.svelte
@@ -5,6 +5,10 @@
 	let shiftX;
 	let shiftY;
 
+	let width;
+	let height;
+	let dx, dy;
+
 	function resizable(node) {
 		pointerTracker = new PointerTracker(node, {
 			eventListenerOptions: { capture: true }, // catch the event before it goes to child in the DOM tree
@@ -12,33 +16,39 @@
 			start: (pointer, event) => {
 				if (pointerTracker.currentPointers.length === 1) return false; // track only 1 pointer at a time
 
-				console.log({ pointer, event });
-				shiftX = pointer.clientX - node.getBoundingClientRect().left;
-				shiftY = pointer.clientY - node.getBoundingClientRect().top;
-
 				event.preventDefault();
 				event.stopPropagation();
+
+				width = (width || getComputedStyle(node.parentNode)['width'].replace('px', '') || 0) * 1;
+				height = (height || getComputedStyle(node.parentNode)['height'].replace('px', '') || 0) * 1;
+
+				shiftX = 0; // pointer.clientX - node.getBoundingClientRect().left;
+				shiftY = 0; // pointer.clientY - node.getBoundingClientRect().top;
+
 				return true;
 			},
 			move: (previousPointers, changedPointers, event) => {
+				event.preventDefault();
 				event.stopPropagation(); // continue exclusive rights over the pointer from DOM tree
 
-				resizePointerMove({
-					pageX: pointerTracker.currentPointers[0].pageX,
-					pageY: pointerTracker.currentPointers[0].pageY
-				});
+				dx = pointerTracker.currentPointers[0].pageX - previousPointers[0].pageX;
+				dy = pointerTracker.currentPointers[0].pageY - previousPointers[0].pageY;
+
+				// const currentRect = node.parentNode.getBoundingClientRect();
+				// pointerTracker.currentPointers[0].pageX - shiftX - currentRect.left + 'px';
+				// pointerTracker.currentPointers[0].pageY - shiftY - currentRect.top + 'px';
+
+				width = width + dx;
+				height = height + dy;
+
+				node.parentNode.style.width = width + 'px';
+				node.parentNode.style.height = height + 1 + 'px';
 			},
 			end: (pointer, event, cancelled) => {
 				// nothing to do here
 				console.log('Drag ended');
 			}
 		});
-
-		function resizePointerMove({ pageX, pageY }) {
-			const currentRect = node.parentNode.getBoundingClientRect();
-			node.parentNode.style.width = pageX - shiftX - currentRect.left + 'px';
-			node.parentNode.style.height = pageY - shiftY - currentRect.top + 'px';
-		}
 	}
 </script>
 
@@ -46,20 +56,20 @@
 
 <style>
 	:root {
-		--width: 2em;
+		--width: 2.5em;
 		--line-width: 0.4em;
 	}
 	.svlt-resizer {
 		user-select: none;
 		padding: 0.5em;
 
-		width: 2.5em;
-		height: 2.5em;
+		width: calc(var(--width) * 2.25);
+		height: calc(var(--width) * 2.25);
 		position: absolute;
 		right: 0;
 		bottom: 0;
 		cursor: se-resize;
-		transform: translate(calc(var(--width) / 2), calc(var(--width) / 2));
+		transform: translate(calc(var(--width) / 1), calc(var(--width) / 1));
 		transform-origin: 0 0;
 	}
 	.svlt-resizer::after {
@@ -71,7 +81,7 @@
 		height: calc(var(--width) / 2);
 		border-right: var(--line-width) solid rgba(107, 107, 107, 0.5);
 		border-bottom: var(--line-width) solid rgba(133, 133, 133, 0.5);
-		transform: translate(calc(-1 * var(--width) / 2), calc(-1 * var(--width) / 2));
+		transform: translate(calc(-1 * var(--width) / 1.1), calc(-1 * var(--width) / 1.1));
 		transform-origin: 0 0;
 	}
 </style>
