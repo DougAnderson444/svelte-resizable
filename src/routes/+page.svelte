b6b70a8c9dc5a87f02697b2f7e7d07953cd8745f
Doug A <douganderson444@gmail.com>
2023-01-09 13:10:11 -0400

    tweak index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,13 +12,13 @@
 >
 
 <div class="flex flex-wrap content-start bg-red-100 p-2 h-screen ">
-	<div class="basis-auto w-1/3 h-1/3 bg-green-100 m-2 p-2" use:resizable>1</div>
-	<div class="basis-auto w-1/4 h-1/3 bg-purple-100 m-2 p-2" use:resizable>2</div>
-	<div class="basis-auto w-1/4 h-1/3 bg-blue-100 m-2 p-2 text-3xl" use:resizable>3</div>
+	<div class="basis-auto w-1/3 h-1/3 bg-green-100 m-2 p-2" use:resizable>Small</div>
+	<div class="basis-auto w-1/4 h-1/3 bg-purple-100 m-2 p-2  text-xl" use:resizable>Med</div>
+	<div class="basis-auto w-1/4 h-1/3 bg-blue-100 m-2 p-2 text-3xl" use:resizable>Large</div>
 	<div
 		class="basis-auto w-1/2 h-1/4 bg-amber-800 m-2 p-2 text-4xl"
 		use:resizable={{ Resizer: CustomResizer }}
 	>
-		💩
+		Custom 💩
 	</div>
 </div>
